3ad9a41dd95c14718c82196f15923c4e4d7a509d
volkanyenibagci <volkanyenibagci@gmail.com>
2022-10-29 21:49:25 +0300

    İnitial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Startup Reference
+var startup = new WebApiLogin.Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+//Startup referance
+startup.Configure(app, builder.Environment); 
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
